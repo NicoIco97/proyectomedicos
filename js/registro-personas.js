f899f4f0172d2d6fdc2e390bbf2b296d23e68c11
Nicolas Ico <iconicolas1201@gmail.com>
2023-05-25 16:41:56 -0500

    subir archivos

--- js/registro-personas.js
+++ js/registro-personas.js
@@ -9,22 +9,22 @@ const formularioRegistro = document.getElementById("registro-personas-form")
 // Creacion del objeto
 formularioRegistro.addEventListener("submit", function(event){
 event.preventDefault()
-const persona = {
-nombrePer: nombrePer.value,
-apellidoPer: apellidoPer.value,
-cedulaPer: cedulaPer.value,
-edadPer: edadPer.value,
-telefonoPer: telefonoPer.value,
-especialidadPer: especialidadPer.value
-}
-// Creacion del Json
-let personas = []
-let localPersonas = localStorage.getItem("personas")
-if (localPersonas){
-personas = JSON.parse(localPersonas)
-}
-personas.push(persona)
-localStorage.setItem("personas", JSON.stringify(personas))
-formularioRegistro.reset()
-alert("Persona registrada exitosamente !!!")
+    const persona = {
+    nombrePer: nombrePer.value,
+    apellidoPer: apellidoPer.value,
+    cedulaPer: cedulaPer.value,
+    edadPer: edadPer.value,
+    telefonoPer: telefonoPer.value,
+    especialidadPer: especialidadPer.value
+    }
+    // Creacion del Json
+    let personas = []
+    let localPersonas = localStorage.getItem("personas")
+    if (localPersonas){
+    personas = JSON.parse(localPersonas)
+    }
+    personas.push(persona)
+    localStorage.setItem("personas", JSON.stringify(personas))
+    formularioRegistro.reset()
+    alert("Persona registrada exitosamente !!!")
 })
